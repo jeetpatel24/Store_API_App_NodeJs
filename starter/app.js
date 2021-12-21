5014fd493300c77376e852f30f46413a26044e20
jeetpatel24 <friend.jeet24@gmail.com>
2021-12-21 17:59:34 +0530

    initial setup

---
+++ starter/app.js
@@ -0,0 +1,38 @@
+//to access anvironment variables
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+//middleware
+app.use(express.json()); //not used in this project
+
+//routes
+app.get('/', (req, res)=> {
+    res.send('<h1>Store API</h1><a href="api/v1/products">products route</a>');
+})
+
+
+//products route
+
+
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        //connectDB
+        app.listen(port, console.log(`server is listening on port ${port}...`))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
